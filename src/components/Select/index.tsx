fbdb6f0d0160592d337773185ebfa22e817e9018
Bgogoi123 <bgogoi055@gmail.com>
2023-05-22 12:59:26 +0530

    [feat][Disease Selection] - Select a particular disease, - Display data based on selected disease.

--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -1,8 +1,52 @@
-function Select() {
+import { useEffect, useState } from "react";
+import { TCity } from "../../types";
+
+function Select({
+  data,
+  setSelectedDisease,
+}: {
+  data: Record<string, TCity>;
+  setSelectedDisease: React.Dispatch<React.SetStateAction<string>>;
+}) {
+  const [diseaseNames, setDiseaseNames] = useState<string[]>([]);
+
+  useEffect(() => {
+    getDiseaseNames();
+  }, []);
+
+  function getDiseaseNames() {
+    Object.keys(data).forEach((city) => {
+      const diseaseCount = data[city].diseasecount;
+      setDiseaseNames([]);
+      Object.keys(diseaseCount).forEach((disease) => {
+        setDiseaseNames((prev) => [...prev, disease]);
+      });
+    });
+  }
+
   return (
-    <div style={{ border: "2px solid red", padding: "0.5em 0 0.5em 0" }}>
-      <select>
-        <option selected> Select a Disease</option>
+    <div style={{ padding: "0.5em 0 0.5em 0" }}>
+      <select
+        placeholder="Select "
+        style={{
+          padding: "0.4em 1em 0.4em 1em",
+          color: "blueviolet",
+        }}
+        onChange={(event) => setSelectedDisease(event.target.value)}
+      >
+        {diseaseNames.map((disease, index) => {
+          return (
+            <option
+              key={index}
+              value={disease}
+              style={{
+                color: "darkblue",
+              }}
+            >
+              {disease}
+            </option>
+          );
+        })}
       </select>
     </div>
   );

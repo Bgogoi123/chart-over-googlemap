142f164a31a83e66e86034182fc2f5b5158fdc15
Bgogoi123 <bgogoi055@gmail.com>
2023-05-25 12:42:32 +0530

    [feat][Map Styles] - Added new map styles as shown in : https://developers.google.com/maps/documentation/javascript/examples/maptype-styled-simple, - Styles yet to be tested, as no proper Google Maps API is availabe at   present. - Removed unused codes (functions, states, styles) from DateScale.

--- src/components/DateScale/index.tsx
+++ src/components/DateScale/index.tsx
@@ -1,12 +1,10 @@
 import { Slider } from "@mui/material";
-import { useEffect, useRef, useState } from "react";
+import { useEffect } from "react";
 import { TDateScaleProps } from "../../types/props";
 import { convertDateToString } from "../../utils/functions";
 import "./styles.css";
 
 function DateScale({ dates, setSelectedDate }: TDateScaleProps) {
-  const [axisWidth, setAxisWidth] = useState(0);
-
   const marks = dates.map((datum, index) => {
     return {
       value: index,
@@ -22,20 +20,6 @@ function DateScale({ dates, setSelectedDate }: TDateScaleProps) {
       });
     }
   }, [marks]);
-
-  useEffect(() => {
-    getSVGWidth();
-  }, []);
-
-  const getSVGWidth = () => {
-    const resize = () => {
-      const svgarea = document.querySelector("#axis-x");
-      const width = svgarea!?.getBoundingClientRect().width;
-      setAxisWidth(width);
-    };
-    window.addEventListener("resize", resize);
-    resize();
-  };
 
   const handleChangeSlider = (value: number | number[]) => {
     setSelectedDate(dates[value as number]);
